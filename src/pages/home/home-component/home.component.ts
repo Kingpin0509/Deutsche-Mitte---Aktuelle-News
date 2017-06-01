b6709913b53e18fef71727c9faacdbf6a8330131
Michael Humann <Kingpin0509@gmail.com>
2017-06-01 15:39:09 +0200

    1

--- src/pages/home/home-component/home.component.ts
+++ src/pages/home/home-component/home.component.ts
@@ -18,34 +18,34 @@ import { YoutubeChannelComponent } from '../../youtube/youtube-channel/youtube-c
 	templateUrl: 'home.html'
 })
 export class HomeComponent {
-	pages: Array<{title: string, component: any, icon: string, note: string, params?: any}>;
+	pages: Array<{ title: string, component: any, icon: string, note: string, params?: any }>;
 	constructor(
 		private navController: NavController,
 		private menuController: MenuController,
-		private events: Events) {}
+		private events: Events) { }
 
 	ngOnInit() {
-	  	this.pages = [
-	      { title: 'LOGIN', component: WordpressHome, icon: 'log-in', note: 'Wordpress' },
-		  { title: 'POSTS', component: WordpressPosts, icon: 'paper', note: 'Wordpress' },
-		  { title: 'CATEGORIES', component: WordpressCategories, icon: 'bookmarks', note: 'Wordpress' },
-		  { title: 'TAGS', component: WordpressTags, icon: 'bookmark', note: 'Wordpress' },
-	      { title: 'FEEDS', component: FeedCategoriesComponent, icon: 'logo-rss', note: 'RSS (YQL)' },
-	      { title: 'FEED_CATEGORY', component: FeedCategoryComponent, icon: 'logo-rss', note: 'RSS (YQL)' },
-	      { title: 'YOUTUBE_CHANNEL', component: YoutubeChannelComponent, icon: 'logo-youtube', note: 'Youtube' },
-//	      { title: 'YOUTUBE_VIDEOS', component: YoutubeVideosComponent, icon: 'logo-youtube', note: 'Youtube' },
-//		  { title: 'CATEGORY', component: WordpressPosts, icon: 'pricetags', note: 'Wordpress', params: { category: { name: 'Category Name', id: 16 }}},
-//	      { title: 'MENUS', component: WordpressMenus, icon: 'menu', note: 'Wordpress' },
-//	      { title: 'Firebase', component: FirebaseHomeComponent, icon: 'flame', note: 'Firebase' },
-//	      { title: 'SLIDES', component: SlidesComponent, icon: 'images', note: 'Welcome Tour' },
-//	      { title: 'CHARTS', component: ChartsComponent, icon: 'pie', note: 'Chart.js' },
-	    ];
+		this.pages = [
+			{ title: 'POSTS', component: WordpressPosts, icon: 'paper', note: 'Wordpress' },
+			{ title: 'CATEGORIES', component: WordpressCategories, icon: 'bookmarks', note: 'Wordpress' },
+			{ title: 'TAGS', component: WordpressTags, icon: 'bookmark', note: 'Wordpress' },
+			{ title: 'YOUTUBE_CHANNEL', component: YoutubeChannelComponent, icon: 'logo-youtube', note: 'Youtube' },
+			//   	  { title: 'LOGIN', component: WordpressHome, icon: 'log-in', note: 'Wordpress' },
+			//   	  { title: 'FEEDS', component: FeedCategoriesComponent, icon: 'logo-rss', note: 'RSS (YQL)' },
+			//   	  { title: 'FEED_CATEGORY', component: FeedCategoryComponent, icon: 'logo-rss', note: 'RSS (YQL)' },
+			//	      { title: 'YOUTUBE_VIDEOS', component: YoutubeVideosComponent, icon: 'logo-youtube', note: 'Youtube' },
+			//		  { title: 'CATEGORY', component: WordpressPosts, icon: 'pricetags', note: 'Wordpress', params: { category: { name: 'Category Name', id: 16 }}},
+			//	      { title: 'MENUS', component: WordpressMenus, icon: 'menu', note: 'Wordpress' },
+			//	      { title: 'Firebase', component: FirebaseHomeComponent, icon: 'flame', note: 'Firebase' },
+			//	      { title: 'SLIDES', component: SlidesComponent, icon: 'images', note: 'Welcome Tour' },
+			//	      { title: 'CHARTS', component: ChartsComponent, icon: 'pie', note: 'Chart.js' },
+		];
 
-	    this.events.subscribe('navigationEvent',(object) => {
-	    	this.menuController.close();
-				if (object.component) {
-					this.navController.push(object.component, object.params);
-				}
+		this.events.subscribe('navigationEvent', (object) => {
+			this.menuController.close();
+			if (object.component) {
+				this.navController.push(object.component, object.params);
+			}
 		});
 	}
 
