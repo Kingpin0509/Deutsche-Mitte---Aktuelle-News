1daa03616831f7537b7ed71664d81ab1686d9359
Michael Humann <Kingpin0509@gmail.com>
2017-06-29 06:20:22 +0200

    0169

--- platforms/windows/www/cordova_plugins.js
+++ platforms/windows/www/cordova_plugins.js
@@ -145,7 +145,8 @@ module.exports.metadata =
     "cordova-plugin-statusbar": "2.2.1",
     "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-x-socialsharing": "5.1.5",
-    "ionic-plugin-keyboard": "2.2.1"
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-crosswalk-webview": "2.3.0"
 };
 // BOTTOM OF METADATA
 });
