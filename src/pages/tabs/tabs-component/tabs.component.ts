fd007065c8f2a3a03ae1ade66dce12a940827e0a
Michael Humann <Kingpin0509@gmail.com>
2017-07-31 01:10:42 +0200

    0110

--- src/pages/tabs/tabs-component/tabs.component.ts
+++ src/pages/tabs/tabs-component/tabs.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
 //import { ContactComponent } from '../../contact/contact-component/contact.component';
 import { WordpressPostsHome } from '../../wordpress/wordpress-posts-home/wordpress-posts-home.component';
 import { WordpressFavorites } from '../../wordpress/wordpress-favorites/wordpress-favorites.component';
+import { YoutubeChannelComponent } from '../../youtube/youtube-channel/youtube-channel.component';
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsComponent {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  //tab1Root: any = HomeComponent;
   tab1Root: any = WordpressPostsHome;
-  tab2Root: any = WordpressFavorites;
+  tab2Root: any = YoutubeChannelComponent;
+  tab3Root: any = WordpressFavorites;
   constructor() {
   }
 }
