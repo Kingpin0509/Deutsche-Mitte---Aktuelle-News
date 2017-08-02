92863b24c1dfa11bf6d4205892582ff8c97d61eb
Michael Humann <Kingpin0509@gmail.com>
2017-08-02 18:01:28 +0200

    1801

--- src/pages/wordpress/wordpress-posts-home/wordpress-posts-home.component.ts
+++ src/pages/wordpress/wordpress-posts-home/wordpress-posts-home.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-
 import { WordpressService } from '../shared/services/wordpress.service';
 import { WordpressPost } from '../wordpress-post/wordpress-post.component';
-
 @Component({
 	templateUrl: './wordpress-posts-home.html',
-	providers: [ WordpressService ]
+	providers: [WordpressService]
 })
 export class WordpressPostsHome implements OnInit {
-
 	posts: any;
 	pageCount: number;
 	category: any;
@@ -19,15 +16,13 @@ export class WordpressPostsHome implements OnInit {
 	search: string;
 	hideSearchbar: boolean;
 	favoritePosts: any;
-
 	constructor(
 		private navParams: NavParams,
 		private wordpressService: WordpressService,
 		private navController: NavController,
 		private loadingController: LoadingController,
 		private toastController: ToastController,
-		private storage: Storage) {}
-
+		private storage: Storage) { }
 	ngOnInit() {
 		this.category = this.navParams.get('category');
 		this.tag = this.navParams.get('tag');
@@ -35,117 +30,104 @@ export class WordpressPostsHome implements OnInit {
 		this.hideSearchbar = true;
 		this.search = '';
 		this.favoritePosts = [];
-	    this.storage.get('wordpress.favorite')
-	    .then(data => {
-	        if(data) {
-	        	this.favoritePosts = JSON.parse(data);
-	        }
-	    });
+		this.storage.get('wordpress.favorite')
+			.then(data => {
+				if (data) {
+					this.favoritePosts = JSON.parse(data);
+				}
+			});
 		this.getPosts();
 	}
-
 	getPosts() {
 		this.pageCount = 1;
-
 		let query = this.createQuery();
 		let loader = this.loadingController.create({
 			content: "Bitte warten...",
-      duration: 10000
+			duration: 10000
 		});
-
 		loader.present();
 		this.wordpressService.getPosts(query)
-		.subscribe(result => {
-			this.posts = result;
-			loader.dismiss();
-		});
+			.subscribe(result => {
+				this.posts = result;
+				loader.dismiss();
+			});
 	}
-
 	getAuthorPosts(author) {
 		this.author = author;
 		this.getPosts();
 	}
-
 	searchPosts() {
-    	this.getPosts();
+		this.getPosts();
 	}
-
 	loadMore(infiniteScroll) {
 		this.pageCount++;
-
 		let query = this.createQuery();
-	  	let loader = this.loadingController.create({
+		let loader = this.loadingController.create({
 			content: "Bitte warten..."
 		});
 		let toast = this.toastController.create({
 			message: "Keine weiteren Beiträge.",
-      duration: 2000
+			duration: 2000
 		});
-
 		loader.present();
 		this.wordpressService.getPosts(query)
-		.subscribe(result => {
-			infiniteScroll.complete();
-			if(result.length < 1) { 
-				infiniteScroll.enable(false);
-				toast.present();
-			} else {
-				this.posts = this.posts.concat(result);
-			}
-		},
-		error => console.log(error),
-    () => loader.dismiss());
+			.subscribe(result => {
+				infiniteScroll.complete();
+				if (result.length < 1) {
+					infiniteScroll.enable(false);
+					toast.present();
+				} else {
+					this.posts = this.posts.concat(result);
+				}
+			},
+			error => console.log(error),
+			() => loader.dismiss());
 	}
-
 	loadPost(post) {
 		this.navController.push(WordpressPost, {
 			post: post
 		});
 	}
-
 	favoritePost(post) {
-	    let newPost:Boolean = true;
-	    let message:string;
-
-	    this.favoritePosts.forEach(favPost => {
-			if(JSON.stringify(favPost) === JSON.stringify(post)) {
+		let newPost: Boolean = true;
+		let message: string;
+		this.favoritePosts.forEach(favPost => {
+			if (JSON.stringify(favPost) === JSON.stringify(post)) {
 				newPost = false;
 			}
-	    });
-	    
-	    if(newPost) {
+		});
+
+		if (newPost) {
 			this.favoritePosts.push(post);
 			this.storage.set('wordpress.favorite', JSON.stringify(this.favoritePosts));
 			message = "Beitrag wurde zu deinen Favoriten hinzugefügt";
-	    } else {
-	    	message = "Beitrag wurde bereits zu deinen Favoriten hinzugefügt";
-	    }
+		} else {
+			message = "Beitrag wurde bereits zu deinen Favoriten hinzugefügt";
+		}
 		let toast = this.toastController.create({
 			message: message,
-            duration: 2000
+			duration: 2000
 		});
-	    toast.present();
+		toast.present();
 	}
-
 	toggleSearchbar() {
 		this.hideSearchbar = !this.hideSearchbar;
 	}
-
 	createQuery() {
-	let query = {};
-	query['page'] = this.pageCount;
-	if(this.search) {
-	 	query['search'] = this.search;
-	}
-	if(this.category) {
-		query['categories'] = this.category.id;
-	}
-	if(this.tag) {
-		query['tags'] = this.tag.id;
-	}
-	if(this.author) {
-		query['author'] = this.author;
-	}
-	return query;
+		let query = {};
+		query['page'] = this.pageCount;
+		if (this.search) {
+			query['search'] = this.search;
+		}
+		if (this.category) {
+			query['categories'] = this.category.id;
+		}
+		if (this.tag) {
+			query['tags'] = this.tag.id;
+		}
+		if (this.author) {
+			query['author'] = this.author;
+		}
+		return query;
 	}
 }
