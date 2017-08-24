5bb40ec63bf42e43c7e7d1a68f39426148f857ab
Michael Humann <Kingpin0509@gmail.com>
2017-08-24 10:49:31 +0200

    1049

---
+++ platforms/android/build/generated/source/buildConfig/x86/release/de/dm/news/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.dm.news;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "de.dm.news";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "x86";
+  public static final int VERSION_CODE = 200004;
+  public static final String VERSION_NAME = "2.00.0";
+}
