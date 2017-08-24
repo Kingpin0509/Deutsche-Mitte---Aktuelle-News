7fbb2bd448cc8a7f40558315b8252a6c7233aafd
Michael Humann <Kingpin0509@gmail.com>
2017-08-24 10:37:29 +0200

    1036

---
+++ platforms/android/build/generated/source/buildConfig/armv7/debug/de/dm/news/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.dm.news;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "de.dm.news";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "armv7";
+  public static final int VERSION_CODE = 200002;
+  public static final String VERSION_NAME = "2.00.0";
+}
