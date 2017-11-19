3b05c68aff9a601bc4eb03e5defb05c2991f56eb
Michael Humann <Kingpin0509@gmail.com>
2017-11-19 17:01:25 +0100

    1701

--- src/pages/settings/settings-component/settings.component.ts
+++ src/pages/settings/settings-component/settings.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { Storage } from "@ionic/storage";
-import { AppVersion } from "@ionic-native/app-version";
+//import { AppVersion } from "@ionic-native/app-version";
 import { TranslateService } from "ng2-translate";
-import { Nav } from "ionic-angular";
-import { FirebaseHomeComponent } from "../../firebase/firebase-home/firebase-home.component";
+//import { FirebaseHomeComponent } from "../../firebase/firebase-home/firebase-home.component";
 @Component({
   templateUrl: "settings.html"
 })
 export class SettingsComponent implements OnInit {
-  nav: Nav;
-  page: any;
-
   language: string;
   checkbox: boolean;
   disabled: boolean;
   constructor(
     private storage: Storage,
-    private translate: TranslateService,
-    private appVersion: AppVersion
-  ) {}
+    private translate: TranslateService
+  ) //private appVersion: AppVersion
+  {
+  }
   ngOnInit() {
     this.storage.get("language").then(value => {
       if (value) {
@@ -26,10 +23,9 @@ export class SettingsComponent implements OnInit {
       } else {
         this.language = "de";
       }
-    })
-
-  };
-
+    });
+    this.seenTutorial();
+  }
 
   selectLanguage() {
     this.storage.set("language", this.language);
@@ -48,8 +44,5 @@ export class SettingsComponent implements OnInit {
   toogleSeenTutorial() {
     this.storage.set("hasSeenTutorial", false);
     this.disabled = true;
-  }
-  pushPageFirebase(page) {
-    this.nav.push(FirebaseHomeComponent);
   }
 }
--- src/pages/settings/settings.module.ts
+++ src/pages/settings/settings.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 import { SharedModule } from "../../app/shared/shared.module";
 import { SettingsComponent } from "./settings-component/settings.component";
-import { AppVersion } from "@ionic-native/app-version";
+//import { AppVersion } from "@ionic-native/app-version";
 
 @NgModule({
   declarations: [SettingsComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, FormsModule,SharedModule],
   exports: [SettingsComponent],
   entryComponents: [SettingsComponent],
-  providers: [AppVersion]
+  //providers: [AppVersion]
 })
 export class SettingsModule {}
